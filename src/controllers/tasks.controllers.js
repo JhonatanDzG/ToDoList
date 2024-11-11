c63eb1c4dd128675bcf2b8ce0e497547847772f3
JhonatanDzG <jhonatandzg@gmail.com>
2024-11-11 16:23:39 -0500

    feat: routes:POST GETALL

---
+++ src/controllers/tasks.controllers.js
@@ -0,0 +1,47 @@
+const pool = require("../db");
+
+const getAllTasks = async (req, res) => {
+try {
+    const allTasks= await pool.query("SELECT * FROM task");
+    res.json(allTasks.rows);
+} catch (error) {
+    console.log(error.message);
+       
+}
+    
+};
+
+const getTask = (req, res) => {
+  res.send("retrieving a single task");
+};
+
+const createTask = async (req, res) => {
+  const { title, description } = req.body;
+
+try {
+    const result = await pool.query(
+        "INSERT INTO task (title, description) VALUES($1, $2) RETURNING *",
+        [title, description]
+      );
+    
+      res.json(result.rows[0]);
+} catch (error) {
+    res.json({error: error.message})
+    
+}
+};
+
+const deleteTask = (req, res) => {
+  res.send("deleting a task");
+};
+const updateTask = (req, res) => {
+  res.send("updating a task");
+};
+
+module.exports = {
+  getAllTasks,
+  getTask,
+  createTask,
+  deleteTask,
+  updateTask,
+};
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,21 +1,24 @@
 const { Router } = require("express");
 
+const pool = require("../db.js");
+const {
+  getAllTasks,
+  getTask,
+  createTask,
+  deleteTask,
+  updateTask,
+} = require("../controllers/tasks.controllers.js");
+
 const router = Router();
 
-router.get("/tasks", (req, res) => {
-  res.send("retrieving a list of tasks");
-});
-router.get("/tasks/01", (req, res) => {
-  res.send("retrieving a single task");
-});
-router.post("/tasks", (req, res) => {
-  res.send("creating a task");
-});
-router.delete("/tasks", (req, res) => {
-  res.send("deleting a task");
-});
-router.put("/tasks", (req, res) => {
-    res.send("updating a task");
-  });
+router.get("/tasks", getAllTasks);
+
+router.get("/tasks/01", getTask);
+
+router.post("/tasks", createTask);
+
+router.delete("/tasks", deleteTask);
+
+router.put("/tasks", updateTask);
 
 module.exports = router;
