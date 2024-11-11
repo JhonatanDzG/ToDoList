8c693c0bfcd2e270e34a8c668bc6c11cf4560bab
JhonatanDzG <jhonatandzg@gmail.com>
2024-11-11 14:34:37 -0500

    let's conect db(pg)

---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/tasks", (req, res) => {
+  res.send("retrieving a list of tasks");
+});
+router.get("/tasks/01", (req, res) => {
+  res.send("retrieving a single task");
+});
+router.post("/tasks", (req, res) => {
+  res.send("creating a task");
+});
+router.delete("/tasks", (req, res) => {
+  res.send("deleting a task");
+});
+router.put("/tasks", (req, res) => {
+    res.send("updating a task");
+  });
+
+module.exports = router;
